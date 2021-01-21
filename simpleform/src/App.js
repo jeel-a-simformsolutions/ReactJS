bb8bd7d7ea533914611cd57898631625ff0b03a7
jeel-a-simformsolutions <75990867+jeel-a-simformsolutions@users.noreply.github.com>
2021-01-21 10:54:16 +0530

    Update App.js

--- simpleform/src/App.js
+++ simpleform/src/App.js
@@ -3,25 +3,46 @@ import React, { useState } from 'react';
 const App = () => {
 
   const [name, setName] = useState("");
+
+  const [lastName, setlastName] = useState('');
   const [fullName, setFullName] = useState();
 
+  const [lastNamenew, setLastNamenew] = useState();
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setFullName(name);
+    setLastNamenew(lastName);
+  }
   const inputEvent = (event) => {
     console.log(event.target.value);
-    setName(event.target.value)
+    setName(event.target.value);
   };
 
-  const onSubmit = () => {
-    setFullName(name);
+  const inputEventTwo = (event) => {
+    setlastName(event.target.value);
   }
+
+
   return (
     <>
-      <div>
-        <h1> Hello {fullName} </h1>
-        <input type='text'
-          placeholder="Enter Your Name"
-          onChange={inputEvent}
-          value={name} />
-        <button onClick={onSubmit}> Click Me 👍</button>
+      <div className="main_div">
+        <form onSubmit={onSubmit}>
+          <div>
+            <h1> Hello {fullName} {lastNamenew} </h1>
+            <input type='text'
+              placeholder="Enter Your Name"
+              onChange={inputEvent}
+              value={name} />
+            <br />
+            <input type='text'
+              placeholder="Enter Your Last Name"
+              onChange={inputEventTwo}
+              value={lastName} />
+            <button onClick={onSubmit}>
+              Click Me 👍
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
