1565a492c7836d48699ed5055721a268f2efd55b
jeel-a-simformsolutions <75990867+jeel-a-simformsolutions@users.noreply.github.com>
2021-01-21 13:55:18 +0530

    Update App.js

--- simpleform/src/App.js
+++ simpleform/src/App.js
@@ -5,41 +5,69 @@ const App = () => {
   const [fullName, setFullName] = useState({
     fname: '',
     lname: '',
+    email: '',
+    phone: '',
   });
 
-  //const [lastName, setlastName] = useState('');
-  //const [fullName, setFullName] = useState();
-  //const [lastNamenew, setLastNamenew] = useState();
+
 
   const inputEvent = (event) => {
     console.log(event.target.value);
     console.log(event.target.name);
 
-    const value = event.target.value;
-    const name = event.target.name;
+    //const value = event.target.value;
+    //const name = event.target.name;
+
+    const { name, value } = event.target;
 
     setFullName((preValue) => {
-
-      //console.log(prevalue);
-      if (name === 'fName') {
-        return {
-          fname: value,
-          lname: preValue.lname,
-        };
-      } else if (name === 'lName') {
-        return {
-          fname: preValue.fname,
-          lname: value,
-        };
+      console.log(preValue);
+      return {
+        ...preValue,
+        [name]: value,
       }
+
+
+      //  if (name === 'fName') {
+      //  return {
+      //  fname: value,
+      //lname: preValue.lname,
+      //email: preValue.email,
+      //phone: preValue.phone,
+      //};
+
+      //} else if (name === 'lName') {
+      //return {
+      //fname: preValue.fname,
+      //lname: value,
+      //email: preValue.email,
+      //phone: preValue.phone,
+      //};
+
+      //} else if (name === 'email') {
+      //return {
+      //fname: preValue.fname,
+      //lname: preValue.lname,
+      //email: value,
+      //phone: preValue.phone,
+      //};
+
+      //} else if (name === 'phone') {
+      //return {
+      //fname: preValue.fname,
+      //lname: preValue.lname,
+      //email: preValue.email,
+      //phone: value,
+      //};
+
+      //}
     });
-    //setName(event.target.value);
+
   };
   const onSubmit = (event) => {
     event.preventDefault();
     alert('form submited');
-    //setFullName(name);
-    //setLastNamenew(lastName);
+
   };
 
   return (
@@ -48,17 +76,33 @@ const App = () => {
         <form onSubmit={onSubmit}>
           <div>
             <h1> Hello {fullName.fname} {fullName.lname} </h1>
+            <p> {fullName.email} </p>
+            <p> {fullName.phone} </p>
             <input type='text'
               placeholder="Enter Your Name"
-              name='fName'
+              name='fname'
               onChange={inputEvent}
               value={fullName.fname} />
             <br />
             <input type='text'
               placeholder="Enter Your Last Name"
-              name='lName'
+              name='lname'
               onChange={inputEvent}
               value={fullName.lname}
+            />
+            <br />
+            <input type='email'
+              placeholder="Enter Your Email Name"
+              name='email'
+              onChange={inputEvent}
+              value={fullName.email}
+            />
+            <br />
+            <input type='number'
+              placeholder="Enter Your Number Name"
+              name='phone'
+              onChange={inputEvent}
+              value={fullName.phone}
             />
             <button onClick={onSubmit}>
               Click Me 👍
