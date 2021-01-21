598ae72b6924689485fffd66db4bcbd39a945142
jeel-a-simformsolutions <75990867+jeel-a-simformsolutions@users.noreply.github.com>
2021-01-21 12:37:33 +0530

    Update App.js

--- simpleform/src/App.js
+++ simpleform/src/App.js
@@ -2,42 +2,64 @@ import React, { useState } from 'react';
 
 const App = () => {
 
-  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState({
+    fname: '',
+    lname: '',
+  });
 
-  const [lastName, setlastName] = useState('');
-  const [fullName, setFullName] = useState();
+  //const [lastName, setlastName] = useState('');
+  //const [fullName, setFullName] = useState();
+  //const [lastNamenew, setLastNamenew] = useState();
 
-  const [lastNamenew, setLastNamenew] = useState();
-  const onSubmit = (event) => {
-    event.preventDefault();
-    setFullName(name);
-    setLastNamenew(lastName);
-  }
   const inputEvent = (event) => {
     console.log(event.target.value);
-    setName(event.target.value);
+    console.log(event.target.name);
+
+    const value = event.target.value;
+    const name = event.target.name;
+
+    setFullName((preValue) => {
+
+      //console.log(prevalue);
+      if (name === 'fName') {
+        return {
+          fname: value,
+          lname: preValue.lname,
+        };
+      } else if (name === 'lName') {
+        return {
+          fname: preValue.fname,
+          lname: value,
+        };
+      }
+    });
+    //setName(event.target.value);
   };
-
-  const inputEventTwo = (event) => {
-    setlastName(event.target.value);
-  }
-
+  const onSubmit = (event) => {
+    event.preventDefault();
+    alert('form submited');
+    //setFullName(name);
+    //setLastNamenew(lastName);
+  };
 
   return (
     <>
       <div className="main_div">
         <form onSubmit={onSubmit}>
           <div>
-            <h1> Hello {fullName} {lastNamenew} </h1>
+            <h1> Hello {fullName.fname} {fullName.lname} </h1>
             <input type='text'
               placeholder="Enter Your Name"
+              name='fName'
               onChange={inputEvent}
-              value={name} />
+              value={fullName.fname} />
             <br />
             <input type='text'
               placeholder="Enter Your Last Name"
-              onChange={inputEventTwo}
-              value={lastName} />
+              name='lName'
+              onChange={inputEvent}
+              value={fullName.lname}
+            />
             <button onClick={onSubmit}>
               Click Me 👍
             </button>
@@ -47,5 +69,6 @@ const App = () => {
     </>
   );
 };
+
 
 export default App;
